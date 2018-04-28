224b79e8bcfec11de1fa466d26acfabac70a72df
Rachel Fine <rachelefine@Rachels-MacBook-Air.local>
2018-04-28 13:50:32 -0400

    still working on adding categories for new users

--- App.js
+++ App.js
@@ -29,12 +29,20 @@ class SignIn extends React.Component {
       });
     })
     .catch(error => console.log(error));
-
   }
 
   handleSignup(event) {
     event.preventDefault();
-    alert('signup!');
+    const email = this.state.email
+    const password = this.state.password
+    axios
+      .post('http://192.168.1.156:8080/auth/signup', { email, password })
+      .then(res => {
+        const user = res.data;
+        this.setState({ user });
+        this.props.navigation.navigate('CategoryPage', { user: this.state.user });
+      })
+      .catch(error => console.log(error));
   }
 
   render() {
@@ -101,11 +109,83 @@ class UserPage extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-          {/*<Image source={this.state.pic} style={{ width: 200, height: 400 }} />*/}
           <WebView source={{ uri: this.state.site }} style={{ marginTop: 20, width: 300, height: 300 }} />
           <Button onPress={this.nextSite.bind(this)} title="Next"/>
       </View>
     )
+  }
+}
+
+
+//ADD CATEGORIES PAGEEEEEEEE
+
+class CategorySelectPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      urlsToAdd: [],
+      user: {}
+    };
+  }
+
+  componentDidMount() {
+    const { params } = this.props.navigation.state;
+    const user = params.user;
+    this.setState({ user });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const urlsToAdd = this.state.urlsToAdd;
+    const userId = this.state.user.id;
+    axios
+      .post(`http://192.168.1.156:8080/api/users/${userId}/categories`, {
+        urlsToAdd
+      })
+      .then(res => {
+        const user = res.data;
+        this.setState({ user });
+        console.log(user)
+        this.props.navigation.navigate('Page', { user: this.state.user });
+      })
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          onPress={() => this.state.urlsToAdd.push(
+            'https://www.vox.com/videos/2017/5/23/15362626/how-tap-dancing-was-made-in-america'
+          )}
+          title="Tap Dancing"
+          color="darkgrey"
+          accessibilityLabel="tap dancing"
+        />
+        <Button
+          onPress={() => this.state.urlsToAdd.push(
+            'http://cheezburger.com/1633029/33-funny-cat-memes-that-never-fail-to-make-us-lol'
+          )}
+          title="Cats"
+          color="darkgrey"
+          accessibilityLabel="cats"
+        />
+        <Button
+          onPress={() => this.state.urlsToAdd.push(
+            'https://www.motherjones.com/media/2014/06/computer-science-programming-code-diversity-sexism-education/'
+          )}
+          title="Coding"
+          color="darkgrey"
+          accessibilityLabel="coding"
+        />
+        <Button
+          onPress={this.handleSubmit.bind(this)}
+          title="Submit"
+          color="darkgrey"
+          accessibilityLabel="submit"
+        />
+      </View>
+    );
   }
 }
 
@@ -117,6 +197,9 @@ const RootStack = StackNavigator(
     Page: {
       screen: UserPage
     },
+    CategoryPage: {
+      screen: CategorySelectPage
+    }
   },
   {
     initialRouteName: 'Home',
