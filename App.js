3ec39d8357146554a399ba885b3515cea361d109
Rachel Fine <rachelefine@Rachels-MacBook-Air.local>
2018-07-28 14:44:12 -0400

    removed comments and some code cleanup

--- App.js
+++ App.js
@@ -1,48 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button, TouchableWithoutFeedback, WebView, KeyboardAvoidingView } from 'react-native';
-import { FormLabel, FormInput } from 'react-native-elements'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableWithoutFeedback,
+  WebView,
+  KeyboardAvoidingView
+} from 'react-native';
+import { FormLabel, FormInput } from 'react-native-elements';
 import { StackNavigator } from 'react-navigation';
 import axios from 'axios';
-import { KeyAwareScrollView, KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import {
+  KeyAwareScrollView,
+  KeyboardAwareScrollView
+} from 'react-native-keyboard-aware-scroll-view';
+import PasswordInputText from 'react-native-hide-show-password-input';
 
-//fullstack IP - http://172.16.26.79:8080
-//hannah IP - http://192.168.1.156:8080
+//GOALS:
+//firebase for authentication and db
+//preselected categories
+//ability to set timer
 
 class SignIn extends React.Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
       email: '',
       password: '',
       user: {}
-    }
+    };
   }
-  
+
   handleLogin(evt) {
     evt.preventDefault();
-    const email = this.state.email
-    const password = this.state.password
+    const email = this.state.email;
+    const password = this.state.password;
     axios
       .post('http://192.168.1.156:8080/auth/login', { email, password })
       .then(res => {
         const user = res.data;
-        this.setState({ user });
-        this.props.navigation.navigate('Page', { user: this.state.user });
+        this.setState({ user }, () => {
+          this.props.navigation.navigate('Page', { user: this.state.user });
+        });
       })
       .catch(error => console.log(error));
   }
 
   handleSignup(event) {
     event.preventDefault();
-    const email = this.state.email
-    const password = this.state.password
+    const email = this.state.email;
+    const password = this.state.password;
     axios
       .post('http://192.168.1.156:8080/auth/signup', { email, password })
       .then(res => {
         const user = res.data;
-        this.setState({ user });
-        this.props.navigation.navigate('CategoryPage', {
-          user: this.state.user
+        this.setState({ user }, () => {
+          this.props.navigation.navigate('CategoryPage', {
+            user: this.state.user
+          });
         });
       })
       .catch(error => console.log(error));
@@ -54,27 +71,39 @@ class SignIn extends React.Component {
         'http://etcalendar.its.txstate.edu/image/6722/take%20a%20study%20break.jpg'
     };
     return (
-      <KeyboardAvoidingView style={styles.container} behavior='position' enabled>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior="position"
+        enabled
+      >
         <View style={styles.logo}>
-          <Image source={pic} style={{ width: 262, height: 150 }} />
+          <Image source={pic} style={styles.image} />
         </View>
         <View style={styles.form}>
-          <FormLabel>email</FormLabel>
-          <FormInput onChangeText={(value) => this.setState({email: value.toLowerCase()})}/>
-          <FormLabel>password</FormLabel>
-          <FormInput onChangeText={(value) => this.setState({password: value.toLowerCase()})}/>
+          <FormLabel>Email</FormLabel>
+          <FormInput
+            onChangeText={value =>
+              this.setState({ email: value.toLowerCase() })
+            }
+          />
+          <View style={styles.password}>
+            <PasswordInputText
+              value={this.state.password}
+              onChangeText={password => this.setState({ password })}
+            />
+          </View>
           <View style={styles.loginSignupBtns}>
             <Button
               onPress={this.handleLogin.bind(this)}
               title="log in"
               color="darkgrey"
-              accessibilityLabel='login'
+              accessibilityLabel="login"
             />
             <Button
               onPress={this.handleSignup.bind(this)}
               title="sign up"
               color="darkgrey"
-              accessibilityLabel='signup'
+              accessibilityLabel="signup"
             />
           </View>
         </View>
@@ -83,125 +112,122 @@ class SignIn extends React.Component {
   }
 }
 
-//USER PAGEEEEEEEEEEEEEE
+//USER PAGE
 
 class UserPage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    const { params } = props.navigation.state;
     this.state = {
-      user: {},
+      user: params.user || {},
       url: '',
-      catArr: [],
-    }
-  }
-  
-  componentDidMount() {
-    const { params } = this.props.navigation.state
-    const user = params.user
-    let catArr = user.categories
-    this.setState({ user, catArr })
-    let category = catArr[Math.floor(Math.random() * catArr.length)];
-    console.log("category is..", category)
-    let index = Math.floor(Math.random()*100)
-    console.log("index is..", index)
-    fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyCpNig0xgMK0kBUvS063nd8EPvc4NTEJ9o&cx=011206717166158345436:d6qgvrrgtnq&q=${category}&num=1&start=${index}`)
-      .then(res => res.json())
-      .then(site => {
-        console.log(site)
-        let url = site.items[0].link
-        console.log("url is...", url)
-        this.setState({ url });
-      })
-      .catch((err) => console.log(err))
+      catArr: params.user.categories || []
+    };
+    this.nextSite = this.nextSite.bind(this);
   }
 
-  nextSite () {
-    catArr = this.state.catArr
+  componentDidMount() {
+    this.nextSite();
+  }
+
+  nextSite() {
+    catArr = this.state.catArr;
     let category = catArr[Math.floor(Math.random() * catArr.length)];
     let index = Math.floor(Math.random() * 100);
-    fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyCpNig0xgMK0kBUvS063nd8EPvc4NTEJ9o&cx=011206717166158345436:d6qgvrrgtnq&q=${category}&num=1&start=${index}`)
+    console.log('category is..', category);
+    fetch(
+      `https://www.googleapis.com/customsearch/v1?key=AIzaSyCpNig0xgMK0kBUvS063nd8EPvc4NTEJ9o&cx=011206717166158345436:d6qgvrrgtnq&q=${category}&num=1&start=${index}`
+    )
       .then(res => res.json())
       .then(site => {
-        let url = site.items[0].link
-        this.setState({ url })
+        let url = site.items[0].link;
+        this.setState({ url });
       })
-      .catch(err => console.log(err))
+      .catch(err => console.log(err));
   }
 
   render() {
     return (
       <View style={styles.container}>
-          <WebView source={{ uri: this.state.url }} style={{ marginTop: 20, width: 300, height: 300 }} />
-          <Button onPress={this.nextSite.bind(this)} title="Next"/>
+        <WebView source={{ uri: this.state.url }} style={styles.webview} />
+        <Button onPress={this.nextSite} title="Next" />
       </View>
-    )
+    );
   }
 }
 
-
-//ADD CATEGORIES PAGEEEEEEEE
+//ADD CATEGORIES PAGE
 
 class CategorySelectPage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    const { params } = props.navigation.state;
     this.state = {
       catOne: '',
       catTwo: '',
       catThree: '',
-      user: {}
+      user: params.user || {}
     };
   }
 
-  componentDidMount() {
-    const { params } = this.props.navigation.state;
-    const user = params.user;
-    this.setState({ user });
-  }
-
   handleSubmit(event) {
-    event.preventDefault()
-    const catsToAdd = []
+    event.preventDefault();
+    const catsToAdd = [];
     if (this.state.catOne !== '') catsToAdd.push(this.state.catOne);
     if (this.state.catTwo !== '') catsToAdd.push(this.state.catTwo);
     if (this.state.catThree !== '') catsToAdd.push(this.state.catThree);
-    const userId = this.state.user.id
-    console.log('cats are ...', catsToAdd)
+    const userId = this.state.user.id;
     axios
       .post(`http://192.168.1.156:8080/api/users/${userId}/categories`, {
         catsToAdd
       })
       .then(res => {
         const user = res.data;
-        this.setState({ user });
-        console.log(user);
-        this.props.navigation.navigate('Page', { user: this.state.user });
+        this.setState({ user }, () => {
+          this.props.navigation.navigate('Page', {
+            user: this.state.user
+          });
+        });
       })
       .catch(error => console.log(error));
   }
 
   render() {
     return (
-      <KeyboardAwareScrollView 
-        style={{backgroundColor: 'bisque'}} 
-        resetScrollToCoords={{x:0, y:0}} 
-        contentContainerStyle={styles.container} 
-        scrollEnabled={false}>
+      <KeyboardAwareScrollView
+        style={styles.categories}
+        resetScrollToCoords={{ x: 0, y: 0 }}
+        contentContainerStyle={styles.container}
+        scrollEnabled={false}
+      >
         <View style={styles.textHeader}>
-          <Text style={{color: 'white', fontSize: 20}}>add some things you like!</Text>
+          <Text style={styles.categoriestText}>add some things you like!</Text>
         </View>
         <View style={styles.form}>
           <FormLabel>1.</FormLabel>
-          <FormInput onChangeText={(value) => this.setState({catOne: value.toLowerCase()})}/>
+          <FormInput
+            onChangeText={value =>
+              this.setState({ catOne: value.toLowerCase() })
+            }
+          />
           <FormLabel>2.</FormLabel>
-          <FormInput onChangeText={(value) => this.setState({catTwo: value.toLowerCase()})}/>
+          <FormInput
+            onChangeText={value =>
+              this.setState({ catTwo: value.toLowerCase() })
+            }
+          />
           <FormLabel>3.</FormLabel>
-          <FormInput onChangeText={(value) => this.setState({catThree: value.toLowerCase()})}/>
+          <FormInput
+            onChangeText={value =>
+              this.setState({ catThree: value.toLowerCase() })
+            }
+          />
           <View style={styles.catSubmitBtn}>
             <Button
               onPress={this.handleSubmit.bind(this)}
               title="onwards >"
               color="darkorange"
-              accessibilityLabel='submit'
+              accessibilityLabel="submit"
             />
           </View>
         </View>
@@ -223,34 +249,40 @@ const RootStack = StackNavigator(
     }
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Home'
   }
 );
 
 export default class App extends React.Component {
-  constructor () {
-    super ();
+  constructor() {
+    super();
     this.state = {
       rendering: true
-    }
+    };
   }
-  componentDidMount() {
-    // setTimeout(() => {
-    //   this.setState({ rendering: false })
-    // }, 10000)
-  }
-  
+  //UNCOMMENT BELOW FOR TIMER FUNCTIONALITY
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.setState({ rendering: false })
+  //   }, 10000)
+  // }
+
   render() {
-    return (
-      this.state.rendering
-      ? <RootStack />
-      : <View style={styles.kickedOut}>
-          <Text>Time to get back to work!</Text>
-        </View>
-    )
+    return this.state.rendering ? (
+      <RootStack />
+    ) : (
+      <View style={styles.kickedOut}>
+        <Image
+          source={{
+            uri:
+              'https://pbs.twimg.com/profile_images/633129398699360256/0tQX81_2_400x400.png'
+          }}
+          style={styles.image}
+        />
+      </View>
+    );
   }
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -271,6 +303,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
+  kickedOutText: {
+    fontSize: 30,
+    padding: 10,
+    color: 'darkgrey'
+  },
   form: {
     width: 300
   },
@@ -289,9 +326,29 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     paddingBottom: 20,
     backgroundColor: 'darkorange',
+    borderRadius: 30,
     width: 300
   },
   catSubmitBtn: {
     paddingTop: 50
+  },
+  image: {
+    width: 262,
+    height: 150
+  },
+  password: {
+    margin: 20
+  },
+  webview: {
+    marginTop: 20,
+    width: 300,
+    height: 300
+  },
+  categories: {
+    backgroundColor: 'bisque'
+  },
+  categoriestText: {
+    color: 'white',
+    fontSize: 20
   }
 });
